4265e188adcfff3d9c2845ed78a457276976296f
Shubham Kumar <shubham1235813@gmail.com>
2026-08-24 14:34:19 +0530

    Add ESLint with TypeScript support

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+]);
